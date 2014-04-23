6af021e2c2a801b0b6806db7012d4df1ef6e867b
alvinhsian@gmail.com <alvinhsian@gmail.com>
2014-04-23 12:44:00 +0800

    Version 2: Add Inner Class WebAppInterface let  it send/receive data by jQuery UI

--- src/com/example/jQueryApp/MyActivity.java
+++ src/com/example/jQueryApp/MyActivity.java
@@ -1,16 +1,19 @@
 package com.example.jQueryApp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
-    static WebView vw;
+    static WebView vw; //定義類別變數使全類別均可存取
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +21,12 @@ public class MyActivity extends Activity {
         findviews();
     }
     private void findviews(){
-         vw = (WebView)findViewById(R.id.webView);
-        String url = "http://10.120.38.1:8080/webcar/carlist.html";
+        vw = (WebView)findViewById(R.id.webView);
+        String url = "http://10.120.38.1:8080/webcar/js_call_android.html";
         WebSettings webSettings = vw.getSettings();
         webSettings.setJavaScriptEnabled(true);
         vw.loadUrl(url);
+        vw.addJavascriptInterface(new WebAppInterface(this), "Android"); //vw is a WebView object.
     }
 
     @Override
@@ -32,5 +36,15 @@ public class MyActivity extends Activity {
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+    public class WebAppInterface{
+        Context context;
+        WebAppInterface(Context context){
+            this.context = context;
+        }
+        @JavascriptInterface
+        public void showToast(String msg){
+            Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+        }
     }
 }
